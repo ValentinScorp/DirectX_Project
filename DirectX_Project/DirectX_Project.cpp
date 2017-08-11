7894c38201c1ea81a9af151ffa57773e460e579f
Valentin <valentin.blo@gmail.com>
2017-08-11 15:05:57 +0300

    Перевел графику в отдельный файл

--- DirectX_Project/DirectX_Project.cpp
+++ DirectX_Project/DirectX_Project.cpp
@@ -2,160 +2,13 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-struct CUSTOMVERTEX {
-	FLOAT X, Y, Z;
-	D3DVECTOR NORMAL;
-	FLOAT    tu, tv;
-};
-#define CUSTOMFVF (D3DFVF_XYZ | D3DFVF_NORMAL | D3DFVF_TEX1)
-
-void initializeGraphics(IDirect3DDevice9* gfxDevice);
-void initializeLight(IDirect3DDevice9* gfxDevice);
-void attachGraphicsToDevice(IDirect3DDevice9* gfxDevice);
-
-
-LPDIRECT3DVERTEXBUFFER9 v_buffer = NULL;
-LPDIRECT3DINDEXBUFFER9 i_buffer = NULL;
+#include "Graphics.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
-#define new_line 123
 
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-IDirect3DDevice9*	pDevice = nullptr;
-IDirect3DTexture9 *g_texture = NULL;
 
-void initializeGraphics(IDirect3DDevice9* gfxDevice)
-{
-	// create the vertices using the CUSTOMVERTEX struct
-	CUSTOMVERTEX vertices[] =
-	{
-		{ -3.0f, -3.0f, 3.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, },    // side 1
-		{ 3.0f, -3.0f, 3.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, },
-		{ -3.0f, 3.0f, 3.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, },
-		{ 3.0f, 3.0f, 3.0f, 0.0f, 0.0f, 1.0f,  1.0f, 1.0f, },
-
-		{ -3.0f, -3.0f, -3.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, },    // side 2
-		{ -3.0f, 3.0f, -3.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, },
-		{ 3.0f, -3.0f, -3.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, },
-		{ 3.0f, 3.0f, -3.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, },
-
-		{ -3.0f, 3.0f, -3.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, },    // side 3
-		{ -3.0f, 3.0f, 3.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, },
-		{ 3.0f, 3.0f, -3.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, },
-		{ 3.0f, 3.0f, 3.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, },
-
-		{ -3.0f, -3.0f, -3.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, },    // side 4
-		{ 3.0f, -3.0f, -3.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, },
-		{ -3.0f, -3.0f, 3.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, },
-		{ 3.0f, -3.0f, 3.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f, },
-
-		{ 3.0f, -3.0f, -3.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, },    // side 5
-		{ 3.0f, 3.0f, -3.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, },
-		{ 3.0f, -3.0f, 3.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, },
-		{ 3.0f, 3.0f, 3.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, },
-
-		{ -3.0f, -3.0f, -3.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, },    // side 6
-		{ -3.0f, -3.0f, 3.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, },
-		{ -3.0f, 3.0f, -3.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, },
-		{ -3.0f, 3.0f, 3.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f, },
-	};
-
-	// create a vertex buffer interface called v_buffer
-	gfxDevice->CreateVertexBuffer(24 * sizeof(CUSTOMVERTEX),
-		0,
-		CUSTOMFVF,
-		D3DPOOL_MANAGED,
-		&v_buffer,
-		NULL);
-
-	VOID* pVoid;    // a void pointer
-
-					// lock v_buffer and load the vertices into it
-	v_buffer->Lock(0, 0, (void**)&pVoid, 0);
-	memcpy(pVoid, vertices, sizeof(vertices));
-	v_buffer->Unlock();
-
-	// create the indices using an int array
-	short indices[] =
-	{
-		0, 1, 2,    // side 1
-		2, 1, 3,
-		4, 5, 6,    // side 2
-		6, 5, 7,
-		8, 9, 10,    // side 3
-		10, 9, 11,
-		12, 13, 14,    // side 4
-		14, 13, 15,
-		16, 17, 18,    // side 5
-		18, 17, 19,
-		20, 21, 22,    // side 6
-		22, 21, 23,
-	};
-
-	// create an index buffer interface called i_buffer
-	gfxDevice->CreateIndexBuffer(36 * sizeof(short),
-		0,
-		D3DFMT_INDEX16,
-		D3DPOOL_MANAGED,
-		&i_buffer,
-		NULL);
-
-	// lock i_buffer and load the indices into it
-	i_buffer->Lock(0, 0, (void**)&pVoid, 0);
-	memcpy(pVoid, indices, sizeof(indices));
-	i_buffer->Unlock();
-}
-
-void initializeLight(IDirect3DDevice9* gfxDevice)
-{
-	D3DLIGHT9 light;    // create the light struct
-	D3DMATERIAL9 material;
-
-	ZeroMemory(&light, sizeof(light));    // clear out the light struct for use
-	light.Type = D3DLIGHT_DIRECTIONAL;    // make the light type 'directional light'
-	light.Diffuse.r = 0.5f;
-	light.Diffuse.g = 0.5f;
-	light.Diffuse.b = 0.5f;
-	light.Diffuse.a = 1.0f;
-	light.Direction.x = 10.0f;
-	light.Direction.y = 1.3f;
-	light.Direction.z = -1.0f;
-
-	light.Ambient = D3DXCOLOR(0.4, 0.4, 0.4, 1.0);
-	light.Diffuse = D3DXCOLOR(1.0, 1.0, 1.0, 1.0);
-	light.Specular= D3DXCOLOR(0.6, 0.6, 0.6, 1.0);
-
-
-	gfxDevice->SetLight(0, &light);    // send the light struct properties to light #0
-	gfxDevice->LightEnable(0, TRUE);    // turn on light #0
-
-	ZeroMemory(&material, sizeof(D3DMATERIAL9));    // clear out the struct for use
-	material.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // set diffuse color to white
-	material.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // set ambient color to white
-	material.Emissive = D3DXCOLOR(0.0f, 0.0f, 0.0f, 1.0f);    // set ambient color to black
-	material.Specular = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);    // set ambient color to white
-	material.Power = 5.0f;
-	gfxDevice->SetMaterial(&material);    // set the globably-used material to &material
-}
-
-void attachGraphicsToDevice(IDirect3DDevice9* gfxDevice)
-{
-	// select the vertex and index buffers to use
-	gfxDevice->SetStreamSource(0, v_buffer, 0, sizeof(CUSTOMVERTEX));
-	gfxDevice->SetIndices(i_buffer);
-}
-
-void loadTexture(IDirect3DDevice9* gfxDevice)
-{
-	HRESULT hr = D3D_OK;
-
-	hr = D3DXCreateTextureFromFile(gfxDevice, L"brown-paper.bmp", &g_texture);
-
-	if (FAILED(hr)) {
-		//printf("Error loading texture %d", hr);
-	}
-}
 
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
@@ -185,33 +38,12 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 	UpdateWindow(hWnd);
 	// --------------directX ------------------------
 
-	IDirect3D9*	pDirect3D = Direct3DCreate9(D3D_SDK_VERSION);
-
-	D3DPRESENT_PARAMETERS d3dpp;
-	ZeroMemory(&d3dpp, sizeof(d3dpp));
-	d3dpp.Windowed = TRUE;
-	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
-	d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
-	d3dpp.Flags = D3DPRESENTFLAG_LOCKABLE_BACKBUFFER;
-
+	createDevice(hWnd);
 	
-	pDirect3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_PUREDEVICE, &d3dpp, &pDevice);
-
-	initializeGraphics(pDevice);
-	loadTexture(pDevice);
-	initializeLight(pDevice);
-
-
-	//pDevice->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_SELECTARG1);
-	//pDevice->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
-	//pDevice->SetTextureStageState(0, D3DTSS_COLORARG2, D3DTA_DIFFUSE);   //Ignored
-
-	pDevice->SetTexture(0, g_texture);
-
-	pDevice->SetRenderState(D3DRS_LIGHTING, TRUE);	
-	pDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
-	pDevice->SetRenderState(D3DRS_AMBIENT, D3DCOLOR_XRGB(50, 50, 50)); 
+	initializeGraphics();
+	loadTexture();
+	initializeLight(); 
+	initializeOther();
 
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
@@ -220,62 +52,11 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		} else {
-			pDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 0), 0.0f, 0);
-			pDevice->Clear(0, NULL, D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
-			pDevice->BeginScene();
-
-			pDevice->SetFVF(CUSTOMFVF);
-
-			D3DXMATRIX matView;
-			D3DXMatrixLookAtLH(&matView,
-				&D3DXVECTOR3(0.0f, 8.0f, 25.0f),    // the camera position
-				&D3DXVECTOR3(0.0f, 0.0f, 0.0f),     // the look-at position
-				&D3DXVECTOR3(0.0f, 1.0f, 0.0f));    // the up direction
-			pDevice->SetTransform(D3DTS_VIEW, &matView);
-
-			D3DXMATRIX matProjection;
-			D3DXMatrixPerspectiveFovLH(&matProjection, D3DXToRadian(45), (FLOAT)SCREEN_WIDTH / (FLOAT)SCREEN_HEIGHT, 1.0f, 100.0f);
-			pDevice->SetTransform(D3DTS_PROJECTION, &matProjection);
-			// set the world transform
-			static float index = 0.0f; 
-			index += 0.03f;
-			D3DXMATRIX matRotateY;
-			D3DXMatrixRotationY(&matRotateY, index);
-			pDevice->SetTransform(D3DTS_WORLD, &(matRotateY));
-			pDevice->SetRenderState(D3DRS_NORMALIZENORMALS, true);
-
-			attachGraphicsToDevice(pDevice);
-
-			// draw the cube
-			pDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 24, 0, 12);
-			
-			pDevice->EndScene();
-			pDevice->Present(NULL, NULL, NULL, NULL);
+			drawAll();
 		}
 	}
 
-	if (g_texture) {
-		g_texture->Release();
-		g_texture = NULL;
-	}
-
-	if (v_buffer) {
-		v_buffer->Release();
-		v_buffer = NULL;
-	}
-	if (i_buffer) {
-		i_buffer->Release();
-		i_buffer = NULL;
-	}
-
-	if (pDevice) {
-		pDevice->Release();
-		pDevice = NULL;
-	}
-	if (pDirect3D) {
-		pDirect3D->Release();
-		pDirect3D = NULL;
-	}
+	clearAll();
 
 	UnregisterClass(L"My DirectX Project", hInstance);
 
--- DirectX_Project/Graphics.h
+++ DirectX_Project/Graphics.h
@@ -2,4 +2,15 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
+void createDevice(HWND hWnd);
+void initializeGraphics();
+void initializeLight();
+void attachGraphicsToDevice();
+
+void initializeOther();
+void loadTexture();
+
+void drawAll();
+
+void clearAll();
 
