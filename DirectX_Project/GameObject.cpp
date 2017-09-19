1a03b0756735d6af110f05287c6a44b94226beaf
Valentin <valentin.blo@gmail.com>
2017-09-19 22:19:34 +0300

    weights computing

--- DirectX_Project/GameObject.cpp
+++ DirectX_Project/GameObject.cpp
@@ -37,28 +37,38 @@ void GameObject::animate()
 
 	for (int i = 0; i < vertPositionsInit.size(); i++) {
 		D3DXVECTOR3 v((float*)&vertPositionsInit[i]);
-		std::vector<Weight> *weights = &vertexWeights[i];
-		//D3DXVECTOR3 finalVecPositin(v.x, v.y, v.z);
+		std::vector<Weight> *weights = &vertexWeights[i];		
 		D3DXVECTOR3 finalVecPositin(0.0f, 0.0f, 0.0f);
+
+		D3DXVECTOR3 vertPos(v);
+		D3DXMATRIX bonemat;
+		D3DXMATRIX bonematsum;
+		D3DXMATRIX deformbonemat;
+		D3DXMatrixIdentity(&deformbonemat);
+		D3DXMatrixIdentity(&bonematsum);
 		for (int j = 0; j < weights->size(); j++) {			
 			Bone* b = (*weights)[j].bone;
 			float weight = (*weights)[j].weight;
 			if (b != nullptr) {
-				D3DXVECTOR3 vertPos(v);
-
-				D3DXMATRIX bonemat = b->GetLocalToWorldMatrix();
-				//float d1 = D3DXMatrixDeterminant(&bonemat);
-				D3DXMatrixInverse(&bonemat, 0, &bonemat);
-				D3DXVec3TransformCoord(&vertPos, &vertPos, &bonemat);
 				
-				D3DXMATRIX deformbonemat = bones[b->index].GetLocalToWorldMatrix();
-				//D3DXMatrixInverse(&deformbonemat, 0, &deformbonemat);
-				D3DXVec3TransformCoord(&vertPos, &vertPos, &b->GetLocalToWorldMatrix());
-				D3DXVec3Scale(&vertPos, &vertPos, weight);
-							
+				bonemat = b->GetLocalToWorldMatrix();				
+				D3DXMatrixInverse(&bonemat, 0, &bonemat);
+
+				bonematsum += bonemat;
+				D3DXVec3TransformCoord(&vertPos, &v, &bonemat);
+
+				deformbonemat += bones[b->index].GetLocalToWorldMatrix() * weight;		
+				
+				D3DXVec3TransformCoord(&vertPos, &vertPos, &deformbonemat);
+				//D3DXVec3Scale(&vertPos, &vertPos, weight);
+								
 				finalVecPositin += vertPos;
+				//break;
 			}
 		}
+		D3DXVec3TransformCoord(&vertPos, &v, &bonematsum);
+		D3DXVec3TransformCoord(&finalVecPositin, &vertPos, &deformbonemat);
+		//finalVecPositin = vertPos;
 		
 		vertexes[i].position.x = finalVecPositin.x;
 		vertexes[i].position.y = finalVecPositin.y;
